b38d60740095771537841100a37a72ca995161b0
HIDE810 <hidetake1231@gmail.com>
2019-05-30 22:48:35 +0900

    Add sprite animation

--- source/main.cpp
+++ source/main.cpp
@@ -1,47 +1,26 @@
 #include <m3dia.hpp>
 
-#include <citro2d.h>
-#include <unistd.h>
+int main() {
+	//set
+    m3d::Applet app;
+    m3d::Screen scr;
+    m3d::Sprite spr;
 
-#define SPRITE_SHEETS 1
-
-static C2D_SpriteSheet spriteSheet[SPRITE_SHEETS];
-int microsecond = 3*100000;
-
-int main(){
-
-  m3d::Applet app;
-
-  gfxInitDefault();
-  C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
-  C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
-  C2D_Prepare();
-
-  C3D_RenderTarget* target;
-  target = C2D_CreateScreenTarget(GFX_TOP   , GFX_LEFT);
-
-  spriteSheet[0] = C2D_SpriteSheetLoad("romfs:/images.t3x");
-  if (!spriteSheet[0]) svcBreak(USERBREAK_PANIC);
-  C2D_Image Image1 = C2D_SpriteSheetGetImage(spriteSheet[0], 0);
-  C2D_Image Image2 = C2D_SpriteSheetGetImage(spriteSheet[0], 1);
-  
-  m3d::Music music("romfs:/bgm.mp3");
-  music.play();
-
- while (aptMainLoop()){
-    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-    C2D_SceneBegin(target);
-	C2D_TargetClear(target, C2D_Color32(0x00, 0x00, 0x00, 0xFF));
-    C2D_DrawImageAt(Image1, 0.0f, 0.0f, 0.0f, NULL, 1.0f, 1.0f);
-	usleep(microsecond);
-	C3D_FrameEnd(0);
-
-	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-    C2D_SceneBegin(target);
-	C2D_TargetClear(target, C2D_Color32(0x00, 0x00, 0x00, 0xFF));
-	C2D_DrawImageAt(Image2, 0.0f, 0.0f, 0.0f, NULL, 1.0f, 1.0f);
-	usleep(microsecond);
-    C3D_FrameEnd(0);
-  }
-  return 0;
+	//image
+    int Table[] = {0, 1, -1};
+    spr.setSpriteSheet("romfs:/images.t3x");
+    spr.Animate(Table, 250, true);
+	
+	//sound
+	m3d::Music music("romfs:/bgm.mp3");
+	music.loop(true);
+	music.play();
+	
+	//main loop
+    while (app.isRunning()) {
+		if (m3d::buttons::buttonPressed(m3d::buttons::Button::Start)) app.exit();
+        spr.updateSprites();
+        scr.drawTop(spr);
+        scr.render();
+    }
 }
